c52bbec6265c6d90f52307b32802fa7c616ab69f
schr476 <48308123+schr476@users.noreply.github.com>
2020-04-06 13:55:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='uw_capstone_2020',
       version='0.0.1',
-      install_requires=['gym','keras','pandas','numpy']  
+      install_requires=['gym','keras','pandas','numpy','tqdm']  
 )
 
