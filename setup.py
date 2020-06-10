4436e087fd95f0567d6b897e7424ad379f89bf26
AlexisMills <awoodwa@uw.edu>
2020-06-10 10:19:29 -0700

    updated README.md

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='uw_capstone_2020',
+      version='0.0.1',
+      install_requires=['gym','keras','pandas','numpy','tqdm','matplotlib', 'scipy', 'pyDOE', 'tensorflow']  
+)
+
